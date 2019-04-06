ee51b272d5637a57243812b8c46c04393fa3f9d0
YJYJ17 <comyj9078@gmail.com>
2019-04-06 16:00:39 +0900

    디비연동

--- server/www/models/buser.js
+++ server/www/models/buser.js
@@ -2,39 +2,43 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema ({
-   email : {type : String, required : true, unique: true},
+   email : {type : String, required : false, unique: true},
    name : String,
    pw : {type : String, required : true},
    phone : {type : String, required : true},
-   addr : {type : String, required : true}
+   address : {type : String, required : true}
 });
 
 userSchema.statics.addbuser = function (payload) {
    // this === Model
-   const buser = new this(payload);
-   return buser.save();
+   const user = new this(payload);
+   return user.save();
 };
 
-userSchema.statics.busercheckid = function (email){
-   return this.findOne({email});
+userSchema.statics.checkbid = function (email){
+	console.log("email: ", email);
+	var test = this.find({email});
+	console.log("test: ", email);
+   return this.find({email});
 };
 
-userSchema.statics.busercheckpw = function (email){
-   return this.findOne({email});
+userSchema.statics.checkbpw = function (pw){
+   return this.find({pw});
 };
 
-userSchema.statics.buserfindAll = function() {
+userSchema.statics.findAll = function(payload) {
 	return this.find({});
 };
 
 
-userSchema.statics.buserdeleteEmail = function (email) {
-  return this.remove({ email });
+userSchema.statics.deleteEmail = function (email) {
+  return this.remove({ "email" : email });
 };
 
 userSchema.statics.buserlogin = function (email,pw) {
 	//console.log("ok");
 	return this.find({email,pw});
+		
 };
 	
 module.exports = mongoose.model('User', userSchema);
