b51b36adcc5f7cdb05308d945ae3079e1f7815d8
dlsgur2845 <37440569+dlsgur2845@users.noreply.github.com>
2019-05-12 20:18:47 +0900

    sih

--- sih/app/app/src/main/java/hyuk/com/fasttrack/MainActivity.java
+++ sih/app/app/src/main/java/hyuk/com/fasttrack/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,16 +23,40 @@ public class MainActivity extends AppCompatActivity {
         phone = intent.getStringExtra("phone");
 
         // fragment 로 전송할 변수
-        final MainFragment mainFragment = new MainFragment();
         Bundle bundle = new Bundle();
         bundle.putString("email", email);
         bundle.putString("phone", phone);
+        final MainFragment mainFragment = new MainFragment();
         mainFragment.setArguments(bundle);
+        final FavoriteFragment favoriteFragment = new FavoriteFragment();
+        favoriteFragment.setArguments(bundle);
 
         // fragment
         FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.fragment, mainFragment);
         fragmentTransaction.commit();
+
+        Button mainButton = (Button) findViewById(R.id.mainButton);
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, mainFragment);
+                fragmentTransaction.commit();
+            }
+        });
+
+        Button favoriteButton = (Button) findViewById(R.id.favoriteButton);
+        favoriteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, favoriteFragment);
+                fragmentTransaction.commit();
+            }
+        });
     }
 }
