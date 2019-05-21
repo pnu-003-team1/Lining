6580f144e27caba763373d558a43f41a2fc4a164
choeunbin <peb1021@gmail.com>
2019-05-21 20:52:54 +0900

    form invalid

--- myapp/myapp/app.js
+++ myapp/myapp/app.js
@@ -17,6 +17,7 @@ var mymenuRouter = require('./routes/mymenu');
 var addmenuRouter = require('./routes/addmenu');
 var successRouter = require('./routes/success');
 var logoutRouter = require('./routes/logout');
+var modifymenuRouter = require('./routes/modifymenu');
 
 var app = express();
 
@@ -49,6 +50,7 @@ app.use('/mymenu' , mymenuRouter);
 app.use('/addmenu' , addmenuRouter);
 app.use('/success' , successRouter);
 app.use('/logout' , logoutRouter);
+app.use('/modifymenu' , modifymenuRouter);
 //app.use('/ajax', ajax);
 
 
